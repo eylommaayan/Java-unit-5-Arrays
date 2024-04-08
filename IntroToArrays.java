b972a8186247b76d82126b58e21c7a5acd3e085b
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-08 10:52:35 +0300

    first commit

---
+++ IntroToArrays.java
@@ -0,0 +1,12 @@
+// מערכים
+
+public class IntroToArrays {
+    public static void main(String[] args) {
+        // String kingdom1 = "Mork";
+        // String kingdom2 = "WeDrink";
+        // String kingdom3 = "Northumbria";
+        // String kingdom4 = "East Israel";
+
+        String[] kingdoms = {"Mork", "WeDrink","Northumbria" ,"Israel" };
+    }
+}
