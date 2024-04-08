ee078f821e602ccb41fb7cb2962694a9dc876fa7
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-08 11:05:25 +0300

    // שימוש במערך שמות בשביל בחירת מושבים לשם

---
+++ NameSeat.java
@@ -0,0 +1,16 @@
+// שימוש במערך שמות בשביל בחירת מושבים לשם
+
+public class NameSeat {
+    
+    public static void main(String[] args) {
+        
+        String[] seats = {"eylon", "", "Refel", "avi", "moshe"};
+        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
+        System.out.println(seats[0]+", you will take seat " + 0);
+        System.out.println(seats[1]+", you will take seat " + 1);
+        System.out.println(seats[2]+", you will take seat " + 2);
+        System.out.println(seats[3]+", you will take seat " + 3);
+        System.out.println(seats[4]+", you will take seat " + 4);
+
+    }
+}
