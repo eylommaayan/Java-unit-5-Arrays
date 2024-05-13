7c1d6dc3a33051baa927c2763c4d1e28f662545b
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-13 15:15:00 +0300

    עדכון

--- ArrayLoopExamples.java
+++ ArrayLoopExamples.java
@@ -23,14 +23,32 @@ public class ArrayLoopExamples {
         // Example 4: Double the elements of an integer array
         int[] values = {3, 5, 7, 9, 11};
         // TODO: Use a for loop to double each element in the values array
+        for (int i = 0; i < values.length; i++) {
+            values[i] = values[i] * 2;  // Double the current element
+        }
+
 
         // Example 5: Capitalize the elements of a string array
         String[] colors = {"red", "green", "blue"};
         // TODO: Use a for loop to capitalize each element in the colors array
+for (int i = 0; i < colors.length; i++) {
+    colors[i] = colors[i].toUpperCase();  // Capitalize the current element
+}
+
 
         // Example 6: Find the index of a specific value in an integer array
         int[] data = {4, 7, 4, 1, 4, 9, 4};
         int targetValue = 4;
         // TODO: Use a for loop to find the index of targetValue in the data array
+
+        int index = -1; // Initialize index to -1 to indicate that the targetValue is not found
+
+// Using a for loop to find the index of the targetValue in the data array
+for (int i = 0; i < data.length; i++) {
+    if (data[i] == targetValue) {
+        index = i;  // Set index to the current position
+        break;      // Exit the loop once the first match is found
+    }
+}
     }
 }
--- IntroToArrays.java
+++ IntroToArrays.java
@@ -1,12 +1,18 @@
-// מערכים
-// מאפשר לשים ערכים במשתנה אחד במקוד הרבה משתנים
+/**  מערכים
+ מאפשר לשים ערכים במשתנה אחד במקוד הרבה משתנים
+ואז אפשר לגשת אליהם לפי ה-אינדקס שלהם
+*/ 
 public class IntroToArrays {
     public static void main(String[] args) {
-        // String kingdom1 = "Mork";
+        String[] kingdoms = {"Mork", "WeDrink","Northumbria" ,"Israel" };
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+        System.out.println(kingdoms[4]);
+
+         // String kingdom1 = "Mork";
         // String kingdom2 = "WeDrink";
         // String kingdom3 = "Northumbria";
         // String kingdom4 = "East Israel";
-
-        String[] kingdoms = {"Mork", "WeDrink","Northumbria" ,"Israel" };
     }
 }
