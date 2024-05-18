82dfbc87571060f37dd5294154fdd74e7ad1aeb0
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-18 21:58:46 +0300

    עדכון

--- מערך דו-מימדי/TDarrays.java
+++ מערך דו-מימדי/TDarrays.java
@@ -1,12 +1,18 @@
+
+import java.util.Arrays;
+
 /**
  * במערל דו מימדי נאחנו יוצרים מערך עם שני סוגריים מרובעים
- * ובראשון זה מספר תורים
- * בשני מספר שורות
+ * ובראשון זה מספר שורות
+ * בשני מספר  האלמנטים בכל שורה
  * @author user
  */
 
 public class TDarrays {
    public static void main(String[] args) {
-       
+       int[][] grades = new int[3][4];
+       System.out.println(Arrays.toString(grades[0]));
+       System.out.println(Arrays.toString(grades[1]));
+       System.out.println(Arrays.toString(grades[2]));
    } 
 }
