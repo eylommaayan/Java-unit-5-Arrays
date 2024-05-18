373b94095694e1b3d55544e45916b84746632401
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-18 21:48:44 +0300

    עדכון מערכים ודו מימד

--- ArrayLoopExamples.java
+++ ArrayLoopExamples.java
@@ -3,7 +3,7 @@
 public class ArrayLoopExamples {
 
     public static void main(String[] args) {
-        // Example 1: Print the elements of an integer array using a for loop
+        // הדפסת הרכיבים של מערך מספרים שלמים באמצעות לולאה עבור
         int[] numbers = {22, 24, 26, 29, 30};
 
         
@@ -11,32 +11,29 @@ public class ArrayLoopExamples {
             System.out.println(numbers[i]);
         }
 
-
-   
-
-        // Example 3: Print every second element of an integer array
+        //  הדפסת כל רכיב שני במערך מספרים שלמים
         int[] grades = {95, 80, 85, 70, 90, 60, 88, 78};
         for(int i=0; i<grades.length; i+=2){
             System.out.println(grades[i]);
         }
 
-        // Example 4: Double the elements of an integer array
+        // הכפלת הרכיבים של מערך מספרים שלמים
         int[] values = {3, 5, 7, 9, 11};
-        // TODO: Use a for loop to double each element in the values array
+    
         for (int i = 0; i < values.length; i++) {
             values[i] = values[i] * 2;  // Double the current element
         }
 
 
-        // Example 5: Capitalize the elements of a string array
+        // שימוש באותיות רישיות ברכיבים של מערך מחרוזות
         String[] colors = {"red", "green", "blue"};
-        // TODO: Use a for loop to capitalize each element in the colors array
+     
 for (int i = 0; i < colors.length; i++) {
-    colors[i] = colors[i].toUpperCase();  // Capitalize the current element
+    colors[i] = colors[i].toUpperCase();  
 }
 
 
-        // Example 6: Find the index of a specific value in an integer array
+        // איתור האינדקס של ערך ספציפי במערך מספרים שלמים
         int[] data = {4, 7, 4, 1, 4, 9, 4};
         int targetValue = 4;
         // TODO: Use a for loop to find the index of targetValue in the data array
