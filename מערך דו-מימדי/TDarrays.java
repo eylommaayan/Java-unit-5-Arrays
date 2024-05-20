0c8b697ac96ab83e20c63619ed6079e21a9fbed1
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-20 22:19:19 +0300

    עדכון

--- מערך דו-מימדי/TDarrays.java
+++ מערך דו-מימדי/TDarrays.java
@@ -1,5 +1,4 @@
 
-import java.util.Arrays;
 
 /**
  * במערל דו מימדי נאחנו יוצרים מערך עם שני סוגריים מרובעים
@@ -10,9 +9,22 @@ import java.util.Arrays;
 
 public class TDarrays {
    public static void main(String[] args) {
-       int[][] grades = new int[3][4];
-       System.out.println(Arrays.toString(grades[0]));
-       System.out.println(Arrays.toString(grades[1]));
-       System.out.println(Arrays.toString(grades[2]));
+    int[][] grades = {
+        {72,74,76,78},
+        {81,80,84,82},
+        {90,89,99,88}
+    };
+    //    int[][] grades = new int[3][4];
+    //    grades[0][0]=85;
+    //    grades[0][2]= 80;
+    //    grades[0][3]=90;
+    //    grades[1][0]=87;
+    //    grades[1][1]= 71;
+    //    grades[1][2]=70;
+    //    grades[2][0]=97;
+    //    grades[2][1]= 82;
+    //    grades[2][2]=80;
+       System.out.println("" + grades[0][0] + "" + grades[0][1] +""+  grades[0][2] );
+    
    } 
 }
