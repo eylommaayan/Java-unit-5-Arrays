90f8f967ca2e78c6f7d59d531a2f95ddebed5878
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-23 09:08:40 +0300

    עדכון ללולאה

--- מערך דו-מימדי/TDarrays.java
+++ מערך דו-מימדי/TDarrays.java
@@ -1,19 +1,37 @@
 
 
 /**
- * במערל דו מימדי נאחנו יוצרים מערך עם שני סוגריים מרובעים
+ * במערל דו מימדי אנחנו יוצרים מערך עם שני סוגריים מרובעים
  * ובראשון זה מספר שורות
  * בשני מספר  האלמנטים בכל שורה
  * @author user
  */
 
-public class TDarrays {
-   public static void main(String[] args) {
-    int[][] grades = {
-        {72,74,76,78},
-        {81,80,84,82},
-        {90,89,99,88}
-    };
+ public class TDarrays {
+    public static void main(String[] args) {
+        int[][] grades = {
+            {72, 74, 76, 78},
+            {81, 80, 84, 82},
+            {90, 89, 99, 88}
+        };
+
+        for (int j = 0; j < grades[0].length; j++) {
+            System.err.print(grades[0][j] + " "); // [0][0]  [0][1]  [0][2] [0][3]
+        }
+        System.err.print("\n");
+
+        for (int j = 0; j < grades[1].length; j++) {
+            System.err.print(grades[1][j] + " "); // [1][0]  [1][1]  [1][2] [1][3]
+        }
+        System.err.print("\n");
+
+        for (int j = 0; j < grades[2].length; j++) {
+            System.err.print(grades[2][j] + " "); // [2][0]  [2][1]  [2][2] [2][3]
+        }
+        System.err.print("\n");
+    }
+}
+
     //    int[][] grades = new int[3][4];
     //    grades[0][0]=85;
     //    grades[0][2]= 80;
@@ -24,7 +42,7 @@ public class TDarrays {
     //    grades[2][0]=97;
     //    grades[2][1]= 82;
     //    grades[2][2]=80;
-       System.out.println("" + grades[0][0] + "" + grades[0][1] +""+  grades[0][2] );
+    //    System.out.println("" + grades[0][0] + "" + grades[0][1] +""+  grades[0][2] );
     
-   } 
-}
+//    } 
+// }
